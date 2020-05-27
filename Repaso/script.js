0ac9520424fe52582d530af980fd878c3eca0721
MJDA7 <mj.deandaabrego@yahoo.com.mx>
2020-05-27 18:19:05 -0500

    Repaso y Objetos de js

---
+++ Repaso/script.js
@@ -0,0 +1,70 @@
+//ECMA 5
+var persona = "Mali";
+//ECMA 6
+let personaLet = ["", 2];//
+const personaConst = "Mali"; //
+personaLet = "Pablo";
+
+//condición
+
+const obj= {};
+const arry = [];
+
+arry.push("Mali");
+//obj.assign();
+/* Tipos de varialbes 
+String "" `` 
+Number 123456789
+Boolean true/false
+!+ = -
+!- = +
+*/
+
+let isFun = true;
+let isRain = true;
+
+/*
+if (isRain){
+    alert("Lleva sombrilla");
+} else {
+    alert("Disfruta tu caminata");
+}
+*/
+function nombreFuncion(parametro){
+
+}
+
+function nombreFuncion(parametro){
+    return "Algo";
+}
+
+let perro = {
+    nombre: "Ayudante de Santa",
+    color: "Cafe",
+    noPatas: 4,
+    tieneDueño: true,
+    ladrar(aQuien, estaDormido) {
+        if(estadormido){
+            return false; //si esta dormido aca también termina el codigo
+        }
+        if(aQuein){
+            return true;
+        }
+            //console.log("guao, guao! quien eres?");
+            return "No hay a quein ladrar";
+            
+    },
+
+};
+
+console.log(perro.noPatas);
+console.log(perro.ladrar());
+
+if  (perro.ladrar("gato") === "esta dormido"){
+        alert("Despierta!");
+    //pedazo de codigo que haga algo si el perro ladra
+} else if(perro.ladrar("gato") === "hay a quien ladrar"){
+    console.log("Hay ese perro siempre de escandaloso");
+} else if(perro.ladrar("gato") === "No hay a quein ladrar"){
+    console.log("Y el perro");
+}
